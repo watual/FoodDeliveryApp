d6e3e49e5428dc077d5ea04aa5f6ff07ee059427
yoon <pyh051920@gmail.com>
2024-06-20 20:48:47 +0900

    🎉 FEAT: 로그인 기능 구현
    CREATE domain/user/dto/LoginRequestDto
    CREATE global/security/JwtAuthenticaitonFilter
        - 로그인 기능 구현
    
    UPDATE global/config/WebSecurityConfig
        - 로그인 필터 적용

--- src/main/java/com/sparta/fooddeliveryapp/global/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/fooddeliveryapp/global/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sparta.fooddeliveryapp.global.config;
 
 import com.sparta.fooddeliveryapp.domain.user.repository.UserRepository;
+import com.sparta.fooddeliveryapp.global.security.JwtAuthenticationFilter;
 import com.sparta.fooddeliveryapp.global.security.JwtAuthorizationFilter;
 import com.sparta.fooddeliveryapp.global.security.JwtUtil;
 import com.sparta.fooddeliveryapp.global.security.UserDetailsServiceImpl;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,6 +38,13 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, userRepository);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         // csrf 설정
         httpSecurity.csrf((csrf) -> csrf.disable());
@@ -47,11 +56,14 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/api/auth/refresh-token").permitAll()
+                        .requestMatchers("/api/users/login").permitAll()
                         .requestMatchers("/error").permitAll()  // 서버 단에서 에러시 에러창 띄워주는 url
                         .anyRequest().authenticated()
         );
 
-        // 로그인 필터 추가시 여기에 필터 관리 내용 추가
+        // 필터관리
+        httpSecurity.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
