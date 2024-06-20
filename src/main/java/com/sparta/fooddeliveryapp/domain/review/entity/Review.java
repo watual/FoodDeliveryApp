52cff2f07e7c4e471af46f694e34fcd2aa61e816
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-06-20 14:09:17 +0900

    초기 설정 테스트

--- src/main/java/com/sparta/fooddeliveryapp/domain/review/entity/Review.java
+++ src/main/java/com/sparta/fooddeliveryapp/domain/review/entity/Review.java
@@ -1,4 +1,12 @@
 package com.sparta.fooddeliveryapp.domain.review.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
 public class Review {
+    @Id
+    Long hel;
 }
--- src/main/java/com/sparta/fooddeliveryapp/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/fooddeliveryapp/domain/review/repository/ReviewRepository.java
@@ -1,4 +1,7 @@
 package com.sparta.fooddeliveryapp.domain.review.repository;
 
-public interface ReviewRepository {
+import com.sparta.fooddeliveryapp.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
