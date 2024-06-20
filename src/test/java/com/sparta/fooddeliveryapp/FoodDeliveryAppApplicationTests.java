cbc993860ae5c0c30b33922eded0c267f2095792
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-06-20 11:12:33 +0900

    프로젝트 초기화

---
+++ src/test/java/com/sparta/fooddeliveryapp/FoodDeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.fooddeliveryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
