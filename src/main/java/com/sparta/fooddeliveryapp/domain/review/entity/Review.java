38c5924114501964ffa5151b2c703d71f41cf34f
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-06-23 01:01:35 +0900

    🎉 FEAT: ERD Entity 반영 & Repository 생성
    UPDATE  domain/user/entity/User
    UPDATE  domain/user/entity/UserStatus
        - 회원가입 필드 ERD 일치시킴
    
    UPDATE  domain/review/entity/Review
    UPDATE  domain/review/repository/ReviewRepository
        - 리뷰 ERD 구현
    ADD domain/store/entity/Store
    ADD domain/store/repository/StoreRepository
    ADD domain/menu/entity/Menu
    ADD domain/menu/repository/MenuRepository
        - 가게, 메뉴 ERD 구현
    ADD domain/order/entity/CartDetail
    ADD domain/order/repository/CartDetailRepository
    ADD domain/order/entity/Orders
    ADD domain/order/repository/OrderRepository
    ADD domain/order/entity/OrderDetail
    ADD domain/order/repository/OrderDetailRepository
        - 장바구니물품, 주문내역, 주문내역물품 ERD 구현

--- src/main/java/com/sparta/fooddeliveryapp/domain/review/entity/Review.java
+++ src/main/java/com/sparta/fooddeliveryapp/domain/review/entity/Review.java
@@ -1,4 +1,38 @@
 package com.sparta.fooddeliveryapp.domain.review.entity;
 
-public class Review {
+import com.sparta.fooddeliveryapp.domain.store.entity.Store;
+import com.sparta.fooddeliveryapp.domain.user.entity.User;
+import com.sparta.fooddeliveryapp.global.common.TimeStamped;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Review extends TimeStamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Store store;
+
+    @Column
+    private Long orderGroup;
+
+    @Column
+    private String content;
+
+    @Column
+    private Long rate;
 }
