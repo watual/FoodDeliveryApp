1f91ded6ccac898c277c274c954f1c0847160059
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-06-23 19:32:55 +0900

    🎉 FEAT: 리뷰 조회
    UPDATE domain/review/controller/ReviewController
    UPDATE domain/review/service/ReviewService
    UPDATE domain/review/repository/ReviewRepository
    UPDATE domain/review/dot/ReviewResponseDto
        - 리뷰 조회 기능 구현

--- src/main/java/com/sparta/fooddeliveryapp/domain/review/controller/ReviewController.java
+++ src/main/java/com/sparta/fooddeliveryapp/domain/review/controller/ReviewController.java
@@ -1,9 +1,11 @@
 package com.sparta.fooddeliveryapp.domain.review.controller;
 
 import com.sparta.fooddeliveryapp.domain.review.dto.ReviewCreateRequestDto;
+import com.sparta.fooddeliveryapp.domain.review.dto.ReviewResponseDto;
 import com.sparta.fooddeliveryapp.domain.review.service.ReviewService;
 import com.sparta.fooddeliveryapp.domain.user.entity.User;
 import com.sparta.fooddeliveryapp.domain.user.entity.UserRoleEnum;
+import com.sparta.fooddeliveryapp.domain.user.repository.UserRepository;
 import com.sparta.fooddeliveryapp.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
@@ -12,25 +14,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/review")
 public class ReviewController {
+
     private final ReviewService reviewService;
+
+    // 임시 user 생성
+    private final UserRepository userRepository;
+    private User getTempUser() {
+        return userRepository.findById(1L).orElse(null);
+    }
 
     @PostMapping
     public ResponseEntity<String> createReview(
 //            @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestBody ReviewCreateRequestDto requestDto) {
-        // 임시 데이터
-        User user = User
-                .builder()
-                .userId(1L)
-                .loginId("testUser")
-                .password("testPWD")
-                .role(UserRoleEnum.USER)
-                .build();
-        reviewService.createReview(user, requestDto);
+        reviewService.createReview(getTempUser(), requestDto);
         return ResponseEntity.status(HttpStatus.OK).body("리뷰 등록 완료");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReviewResponseDto>> getReviews(
+//            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        return reviewService.getReviews(getTempUser());
     }
 }
